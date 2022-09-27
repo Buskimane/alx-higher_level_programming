152672c56d265065849a3b06ccf6135dd2086cfd
Buskimane <adebusolaemmanuel@gmail.com>
2022-09-27 23:38:56 +0000

    first

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0]
+    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
