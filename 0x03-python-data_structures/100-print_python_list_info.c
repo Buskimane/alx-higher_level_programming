436dea8e6725b6a5175b7f7a41a45bc3210108aa
Buskimane <adebusolaemmanuel@gmail.com>
2022-09-28 00:45:20 +0100

    first

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,67 +1,23 @@
-#include "lists.h"
-#include <stdio.h>
+#include "Python.h"
 
-int is_palindrome(listint_t **head)
+void print_python_list_info(PyObject *p)
 {
-listint_t *nhead, *tort, *hare, *ptort;
-listint_t *cut = NULL, *half, *it1, *it2;
+        PyListObject *list;
+        Py_ssize_t size, i;
+        PyObject *object;
+        struct _typeobject *type;
 
-if (!head || !*head)
-return (1);
-
-nhead = *head;
-if (nhead->next != NULL)
-{
-for (hare = nhead, tort = nhead; hare != NULL && hare->next != NULL;
-ptort = tort, tort = tort->next)
-hare = hare->next->next;
-
-if (hare != NULL)
-{
-cut = tort;
-tort = tort->next;
-}
-ptort->next = NULL;
-half = tort;
-it1 = reverse_listint(&half);
-
-for (it2 = *head; it2; it1 = it1->next, it2 = it2->next)
-{
-if (it2->n != it1->n)
-return (0);
-}
-if (cut == NULL)
-ptort->next = half;
-else
-{
-ptort->next = cut;
-cut->next = half;
-}
-}
-return (1);
-}
-
-/**
- * reverse_listint - Reverses a linked list in pladce
- *
- * @head: Pointer to a pointer pointing to the first item in the list
- *
- * Return: The new head of the reversed list
- */
-
-listint_t *reverse_listint(listint_t **head)
-{
-listint_t *next = NULL, *prev = NULL;
-if (!head || !*head)
-return (NULL);
-
-while ((*head)->next)
-{
-next = (*head)->next;
-(*head)->next = prev;
-prev = *head;
-*head = next;
-}
-(*head)->next = prev;
-return (*head);
+        if (strcmp(p->ob_type->tp_name, "list") == 0)
+        {
+               list = (PyListObject *)p;
+               size = list->ob_base.ob_size;
+               printf("[*] Size of the Python List = %ld\n", size);
+               printf("[*] Allocated = %ld\n", list->allocated);
+               for (i = 0; i < size; i++)
+	       {
+	               object = list->ob_item[i];
+	               type = object->ob_type;
+	               printf("Element %ld: %s\n", i, type->tp_name);
+	       }
+        }
 }
